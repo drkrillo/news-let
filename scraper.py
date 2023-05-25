7b2c623e375e53bbab934506981c8e154737a5ef
Juan Pasutti <63800323+drkrillo@users.noreply.github.com>
2023-05-25 06:27:21 -0300

    added scraper class

--- scraper.py
+++ scraper.py
@@ -1,28 +1,56 @@
 from bs4 import BeautifulSoup
 import requests
-import pdf_reader
 
-SEARCH_TOPICS = [
-    'python',
-]
+
 NUM_PAPERS = 30
 topic = 'music'
-paper_id = 0
-
-URL = f'https://arxiv.org/search/?query={topic}&searchtype=all&source=header&order=-announced_date_first&size=50&abstracts=show&start={paper_id}'
 
 
-page = requests.get(URL)
+class ArxivScraper(object):
+    """
+    Scrapper class for Arxiv scrapers.
+    """
+    def __init__(
+        self, 
+        topic: str,
+        num_papers: int=50,
+        initial_paper: int=0, 
+    ):
+        self.topic = topic
+        self.num_papers = num_papers
+        self.initial_paper = initial_paper
+        self.url = f'https://arxiv.org/search/?query={self.topic}&searchtype=all&source=header&order=-announced_date_first&size=50&abstracts=show&start={self.initial_paper}'
+        self.papers = list()
+    
+    def scrape(self):
+        """
+        Scrapes N papers from Arxiv.
+        Returns dict with:
+        * Title
+        * Abstract
+        * Link to full paper
+        * Summary (empty: will be populated later)
+        """
+        page = requests.get(self.url)
+        soup = BeautifulSoup(page.content, "html.parser")
 
-soup = BeautifulSoup(page.content, "html.parser")
-results = soup.find(id="main-container")
-paper_elements = results.find_all("li", class_="arxiv-result")
+        results = soup.find(id="main-container")
+        paper_elements = results.find_all("li", class_="arxiv-result")
+        paper_elements = paper_elements[self.initial_paper:self.num_papers]
+        for paper in paper_elements:
+            title = paper.find("p", class_="title is-5 mathjax").text
+            abstract = paper.find("span", class_="abstract-full has-text-grey-dark mathjax").text
+            link_to_full_paper = paper.select_one("a[href*=pdf]")["href"]
 
-parsed_papers = []
-for paper in paper_elements:
-    title = paper.find("p", class_="title is-5 mathjax").text
-    abstract = paper.find("span", class_="abstract-full has-text-grey-dark mathjax").text
-    link_to_full_paper = paper.select_one("a[href*=pdf]")["href"]
-    full_article = pdf_reader.extract_pdf_by_url(link_to_full_paper)
+            parsed_paper = {
+                'title': title,
+                'abstract': abstract,
+                'link': link_to_full_paper,
+                'summary': '',
+            }
 
-    print(full_article)
+            self.papers.append(parsed_paper)
+            self.initial_paper += 1
+            self.num_papers -= 1
+
+        return self.papers
